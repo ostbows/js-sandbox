da1282e7c022a45fb92894c9d44b0b8367e507e1
Robin Sjögren <rsjogren@Robins-MacBook-Pro.local>
2019-04-07 12:09:59 +0200

    pathfinding and obstacles

--- public/js/index.js
+++ public/js/index.js
@@ -10,12 +10,12 @@ const easystar = new EasyStar.js();
 const tileWidth = 64;
 const tileHeight = 64;
 
-let player, pathId;
-const tweens = [];
+let player, pathId, pathIv;
+let cancelPath = false;
 
 loader
   .add('assets/captain_america.png')
-  .add('assets/square.jpg')
+  .add('assets/white_square.jpg')
   .load(setup);
 
 function setup() {
@@ -33,35 +33,61 @@ function update(delta) {
 
 function eventsInit() {
   app.renderer.plugins.interaction.on('pointerup', function(event) {
-    const {x, y} = event.data.global;
+    if (!pathId) return findPath(event.data.global);
+
+    cancelPath = true;
     easystar.cancelPath(pathId);
-    pathId = easystar.findPath(
-      Math.floor(player.x/tileWidth),
-      Math.floor(player.y/tileHeight),
-      Math.floor(x/tileWidth),
-      Math.floor(y/tileHeight),
-      function(path) {
-        if (!path) return;
-        tweenPath(path);
-      });
+
+    clearInterval(pathIv);
+    pathIv = setInterval(function() {
+      if (!pathId) {
+        clearInterval(pathIv);
+        findPath(event.data.global);
+      }
+    }, 50);
+  });
+
+  hotkeys('space', function(event) {
+    event.preventDefault();
+    cancelPath = true;
+    easystar.cancelPath(pathId);
   });
 }
 
+function findPath(destination) {
+  pathId = easystar.findPath(
+    Math.floor(player.x/tileWidth),
+    Math.floor(player.y/tileHeight),
+    Math.floor(destination.x/tileWidth),
+    Math.floor(destination.y/tileHeight),
+    function(path) {
+      if (path) tweenPath(path);
+    });
+}
+
 function tweenPath(path) {
-  path.splice(0,1);
-  if (!path.length) return;
-  const coords = {x: player.x, y: player.y};
-  const tween = new TWEEN.Tween(coords)
-    .to({x: path[0].x*tileWidth+32, y: path[0].y*tileHeight+32}, 1000)
-    .onUpdate(function() {
-      player.position.set(coords.x, coords.y);
-    })
-    .onComplete(function() {
-      tweens.splice(0,1);
-      tweenPath(path);
-    })
-    .start();
-  tweens.push(tween);
+  path.shift();
+  if (path.length) {
+    const coords = {x: player.x, y: player.y};
+    const tween = new TWEEN.Tween(coords)
+      .onUpdate(function() {
+        player.position.set(coords.x, coords.y);
+      })
+      .onComplete(function() {
+        if (cancelPath) {
+          cancelPath = false;
+          pathId = null;
+        } else {
+          tweenPath(path);
+        }
+      });
+    tween.to({
+      x: path[0].x*tileWidth+tileWidth/2,
+      y: path[0].y*tileHeight+tileHeight/2
+    }, 1000).start();
+  } else {
+    pathId = null;
+  }
 }
 
 function squaresInit() {
@@ -71,12 +97,18 @@ function squaresInit() {
   for (let i = 0; i < squaresY; i++) {
     const row = [];
     for (let j = 0; j < squaresX; j++) {
-      const square = new Sprite(resources['assets/square.jpg'].texture);
+      let square;
+      if (j % 3 === 1 && Math.random() > 0.7) {
+        square = new Sprite();
+        row.push(1);
+      } else {
+        square = new Sprite(resources['assets/white_square.jpg'].texture);
+        row.push(0);
+      }
       square.width = tileWidth;
       square.height = tileHeight;
       square.position.set(j*tileWidth, i*tileHeight);
       app.stage.addChild(square);
-      row.push(0);
     }
     grid.push(row);
   }
@@ -87,7 +119,7 @@ function squaresInit() {
 function playerInit() {
   player = new Sprite(resources['assets/captain_america.png'].texture);
   player.anchor.set(0.5);
-  player.position.set(32, 32);
+  player.position.set(tileWidth/2, tileHeight/2);
   player.width = 64; player.height = 64;
   app.stage.addChild(player);
 }
